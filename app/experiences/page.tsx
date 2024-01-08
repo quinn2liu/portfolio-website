6dc87cf0d5310a3ae0bc4a6a647d33971de92ce1
quinn2liu <quinn2liu@gmail.com>
2024-01-07 23:50:05 -0500

    experiences (wit mobile) done

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
-type ExListItemProps = {
+type ListItemProps = {
   title: string;
   subtitle: string;
   date: string;
@@ -17,20 +17,20 @@ type ExListItemProps = {
   lightColor: string;
 };
 
-const ListItem = ({title, subtitle, date, content, image, darkColor, lightColor}: ExListItemProps) => {
+const ListItem = ({title, subtitle, date, content, image, darkColor, lightColor}: ListItemProps) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div onClick={() => setIsOpen(!isOpen)} className={`mb-6 sm:p-6 p-4 flex flex-col space-y-3 justify-start ${lightColor} ${darkColor} w-full transition-all duration-500 ${isOpen ? ' h-72' : 'h-32'} overflow-visible`}>
-            <div className="flex flex-row items-start justify-between">
-                <div className="flex flex-row space-x-4">
-                  <img src={image} alt="" className="h-8"/>
-                  <p className="sm:text-3xl text-xl font-bold">{title}</p>
+        <div onClick={() => setIsOpen(!isOpen)} className={`mb-6 sm:p-6 p-3 flex flex-col sm:space-y-3 justify-start ${lightColor} ${darkColor} w-full transition-all duration-500 ${isOpen ? 'h-72' : 'sm:h-32 h-24'} overflow-visible sm:pointer-events-auto pointer-events-none`}>
+            <div className="flex sm:flex-row flex-col items-start justify-between">
+                <div className="flex flex-row sm:items-start items-center space-x-3 sm:mb-0 mb-3">
+                  <img src={image} alt="" className="sm:h-8 h-5"/>
+                  <p className="sm:text-3xl text-lg font-bold">{title}</p>
                 </div>
                 <p className="sm:text-xl text-md font-semi">{date}</p>
             </div>
-            <p className="sm:text-xl text-md font-semi">{subtitle}</p>
-            <div className={`${inter.className} flex-grow text-lg font-normal transition-all duration-500 ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-40'}`}>
+            <p className="sm:text-xl text-md sm:font-semi font-bold sm:mb-0 mb-3">{subtitle}</p>
+            <div className={`${inter.className} flex-grow sm:text-lg text-xs font-normal transition-all duration-500 ${isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-40'}`}>
                 {content}
             </div>
         </div>
@@ -39,8 +39,8 @@ const ListItem = ({title, subtitle, date, content, image, darkColor, lightColor}
 
 const WorkSection = () => {
   return (
-    <div className="pt-8">
-      <div className="mb-4 text-3xl sm:text-5xl">WORK</div>
+    <div className="sm:pt-8">
+      <div className="mb-4 text-2xl sm:text-5xl">WORK</div>
       <ListItem title={"Red Door Design + Staging"} subtitle={"Freelance Application Developer"} date={"January 2024 - Present"} image = {'/red-door.png'} darkColor="dark:bg-teal-600" lightColor='bg-indigo-300' 
         content = {
           <>
@@ -48,7 +48,7 @@ const WorkSection = () => {
               <li>{'•	Developing an end-to-end mobile application using React Native (Expo.js) and Firebase to create a user-friendly inventory management application for 50+ active users.'}</li>
               <li>{'•	Implementing Firebase operations for efficient CRUD functionalities in real time database'}</li>
               <li>{'•	Facilitating regular developer-client meetings to ensure a comprehensive custom solution.'}</li>
-              <li><Link href="/projects/red-door"><strong>{'View Project Case Study >>'}</strong></Link></li>
+              <li><Link href="/projects/red-door"><strong>{'Project Case Study >>'}</strong></Link></li>
             </ul>
           </>
         }
@@ -58,7 +58,7 @@ const WorkSection = () => {
         <>
           <ul className="space-y-2">
               <li>•	Created and analyzed Python script tests for all UI functionality in Symbotic stack as a part of UI Test Automation team. Applications built on .NET and WPF frameworks & tested using SmartBear TestComplete.</li>
-              <li>{'•	Developed a PowerShell script for seamless integration with Symbotic’s Automated Test Framework, automating UI test execution and logging, improving test execution efficiency by ~75%.'}</li>
+              <li>{'•	Developed PowerShell script for seamless integration with Symbotic’s Automated Test Framework. This automated UI test execution, improving test execution efficiency by ~75%.'}</li>
               <li>{'•	Created 40% of Breakpack functionality testing methods in the Symbotic tech stack.'}</li>
           </ul>
         </>} />
@@ -68,13 +68,13 @@ const WorkSection = () => {
 
 const EducationSection = () => {
     return (
-      <div className="pt-8 mb-6 flex flex-col space-y-3 justify-start w-full">
-        <p className="text-3xl sm:text-5xl">EDUCATION</p>
-        <div className="sm:p-6 p-4 flex flex-col space-y-3 justify-start border-4 border-amber-100">
-              <div className="flex flex-row items-center justify-between">
-                  <div className="flex flex-row space-x-4">
-                    <img src="/penn.png" alt="" className="h-8"/>
-                    <p className="sm:text-3xl text-xl font-bold">University of Pennsylvania</p>
+      <div className="sm:pt-8 mb-6 flex flex-col space-y-3 justify-start w-full">
+        <p className="text-2xl sm:text-5xl">EDUCATION</p>
+        <div className="sm:p-6 p-3 flex flex-col justify-start border-4 border-amber-100">
+              <div className="flex sm:flex-row flex-col items-start justify-between">
+                  <div className="flex flex-row space-x-3 mb-3">
+                    <img src="/penn.png" alt="" className="sm:h-8 h-5"/>
+                    <p className="sm:text-3xl text-lg font-bold">University of Pennsylvania</p>
                   </div>
                   <p className="sm:text-xl text-md font-semi">2021 - 2025</p>
               </div>
@@ -89,21 +89,21 @@ const EducationSection = () => {
 
 export default function Experiences() {
   return (
-    <div className="flex flex-col justify-between min-h-screen">
-      <div className="h-full container mx-auto max-w-screen-lg px-6" suppressHydrationWarning>
-        <div className="sm:pt-16 pt-10 flex flex-col justify-start items-start">
-            <div className="mb-4 text-3xl sm:text-7xl">EXPERIENCES👔</div>
-            <Navigation />
-            <BackHome />
-            <WorkSection />
-            <EducationSection />
+    <div>
+      <div className=" flex flex-col justify-between items-start sm:h-full h-screen">
+        <div className="container mx-auto max-w-screen-lg sm:px-6 px-5" suppressHydrationWarning>
+          <div className="mb-4 text-3xl sm:text-7xl sm:pt-16 pt-10">EXPERIENCES👔</div>
+          <Navigation />
+          <BackHome />
+          <div className=" flex flex-col justify-start items-center">
+              <WorkSection />
+              <EducationSection />
+              <a href = "/Quinn Liu Resume.pdf" target="_blank" rel="noopener noreferrer" className="sm:mt-12 mt-10 sm:p-6 p-4 mb-16 flex flex-col justify-center max-w-sm items-center border-4 dark:border-amber-100 border-gray-950">For more details, here's my resumé</a>
+          </div>
         </div>
-      </div>
-      <div className="mt-4 mb-8 flex justify-center items-center">
-        <a href = "/Quinn Liu Resume.pdf">For more details, here's my resumé </a>
       </div>
       <Footer />
     </div>
-    )
+  )
 }
 
