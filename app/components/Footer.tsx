c71f79027bada7cfdf286d353b74336582897ec4
Quinn Liu <quinn2liu@gmail.com>
2025-08-29 21:23:02 -0400

    fix: new resume

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <div className="sm:pb-8 pb-6 sm:text-sm text-xs flex flex-col justify-center items-center">
-            Made with 🗿 by Quinn Liu (Last Update: 12/31/2024) 
+            Made with 🗿 by Quinn Liu (Last Update: 08/29/2025) 
         </div>
     )
 }
